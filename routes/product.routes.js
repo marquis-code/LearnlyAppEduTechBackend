2ed833b145e97128d47a6fd3f14c63fe04c8ba40
Marquis Abah <marquis.abah@shuttlers.ng>
2024-08-05 23:04:27 +0100

    get all products

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -6,6 +6,7 @@ const productController = require('../controllers/product.controller');
 
 router.post('/create',verifyUser, upload.single('file'), productController.createProduct);
 router.get('/', verifyUser, productController.getProducts);
+router.get('/all-products', productController.getProducts);
 router.get('/:id', verifyUser, productController.getProductById);
 router.patch('/:id', verifyUser, upload.single("file"), productController.updateProduct);
 router.delete('/:id', verifyUser, productController.deleteProduct);
